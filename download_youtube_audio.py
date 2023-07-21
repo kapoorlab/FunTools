c2e9c5873fb48d3a74d886b7d7ca9bf7c08725b9
kapoorlab <randomaccessiblekpaoor@gmail.com>
2023-07-21 18:41:27 +0200

    speech recognition

---
+++ download_youtube_audio.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+
+
+
+def download_audio(video_url):
+    video = YouTube(video_url)
+    audio = video.streams.filter(only_audio = True).first()
+
+    try:
+        audio.download("./audio")
+    except:
+        print("Failed to download audio")
+
+    print("audio was downloaded successfully") 
+    
+    
+if __name__ == '__main__':
+    
+    video_url =  'https://www.youtube.com/watch?v=SnWUZ1mdk3Y&t=155s'
+    download_audio(video_url)    
