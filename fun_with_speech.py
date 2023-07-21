c24786204ff734e1a8840c8bdeaa83e6df271040
kapoorlab <randomaccessiblekpaoor@gmail.com>
2023-07-21 18:43:00 +0200

    speech recognition

--- fun_with_speech.py
+++ fun_with_speech.py
@@ -1,6 +1,6 @@
 import speech_recognition as sr
 from playsound import playsound
-from pytube import YouTube
+
 def main(sound_file):
        
        r = sr.Recognizer()
@@ -9,18 +9,8 @@ def main(sound_file):
        with audio_file as source:
            audio = r.record(source)
        text = r.recognize_google(audio) 
-       print(text)
+       print(f'The audio that you just heard said: {text}')
        
-def download_audio(video_url):
-    video = YouTube(video_url)
-    audio = video.streams.filter(only_audio = True).first()
-
-    try:
-        audio.download("./audio")
-    except:
-        print("Failed to download audio")
-
-    print("audio was downloaded successfully")    
        
 if __name__=="__main__":
    
